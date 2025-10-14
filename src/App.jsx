c7d5fb1dd6fdeafb1ac23f8ced1b2998d1f3bbd5
Jonizz14 <jahongirtuhtaev989@gmail.com>
2025-10-14 17:49:19 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import home from "./components/home"
+
+function App() {
+  <home />
+}
+
+export default App
---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function home() {
+  return (
+    <div>
+      ishla
+    </div>
+  )
+}
+
+export default home
